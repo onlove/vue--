c5f26456824e600b35530faf7349560ef1bbdbe6
hulinhai <hulinhai@derbysoft.com>
2017-04-01 17:54:11 +0800

    miaov vue.2.0.js base and example

---
+++ miaov/实例/app.js
@@ -0,0 +1,96 @@
+/**
+ * Created by DT274 on 2017/4/1.
+ */
+var list = [
+    {
+        title: "吃饭睡觉打豆豆1",
+        isChecked: false
+    },
+    {
+        title: "吃饭睡觉打豆豆2",
+        isChecked: true
+    }
+];
+
+
+new Vue({
+    el: ".main",
+    data: {
+        list: list,
+        todo: '',
+        edtorTodos: '',  //记录编辑的数据
+        beforeTitle: ''  //正在编辑的title
+    },
+    computed: {
+        noCheckLength(){
+            return this.list.filter(function(item){
+                return !item.isChecked
+            }).length
+        }
+    },
+    methods: {
+        addTodo() {
+            /* 添加一项任务 */
+            this.list.push({
+                title: this.todo,
+                isChecked: false
+            });
+            this.todo = '';
+        },
+        deleteTodo(todo) {
+            var index = this.list.indexOf(todo);
+            this.list.splice(index, 1);
+        },
+        edtorTodo(todo) {
+            //编辑任务的时候，记录一下编辑这条任务的title,方便在取消的时候重新赋给之前的title
+            this.beforeTitle = todo.title;
+            this.edtorTodos = todo;
+
+        },
+        edtoredTodo() {
+            this.edtorTodos = '';
+        },
+        cancelTodo(todo){
+           todo.title = this.beforeTitle;
+           //div显示出来,input隐藏
+           this.beforeTitle = '';
+           this.edtorTodos = '';
+        }
+    },
+    directives: {
+        'foucs': {
+            //bind: function(el, binding){
+            //    console.log('bind:',binding.value);
+            //},
+            //inserted: function(el, binding){
+            //    console.log('insert:',binding.value);
+            //},
+            update(el, binding) {
+                //console.log(el);
+                //console.log(binding);
+                if(binding.value){
+                    el.focus();
+                }
+            }
+        }
+    }
+    /*
+    * 事件修饰符
+    *   事件处理函数只有纯粹的逻辑判断，不处理DOM事件的细节，例如：阻止冒泡，取消默认行为，判断按钮
+    *   修饰符的位置:
+    *       v-on:eventName.修饰符
+    *   修饰符:
+    *        .stop       阻止单击事件冒泡
+    *        .capture    事件捕获模式
+    *        .self       只当事件在该元素本身触发时触发回调
+    *        .prevent    取消默认行为
+    *        .once       只触发一次事件
+    *
+    *    按键修饰符:
+    *       .enter      .tab      .delete    .esc
+    *       .space      .up       .down      .left
+    *       .right      .ctrl     .alt       .shift
+    *       .meta       .键值
+    *
+    * */
+});
